6fde79f811288aa26e07f17e66a0bbd8887cfc7c
anshkumar20190199328-ops <ansh.kumar20190199328@gmail.com>
2026-07-11 17:16:15 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "todo-pro-v1";
+const CACHE_NAME = "todo-pro-v2";
 
 const urlsToCache = [
   "./",
@@ -6,21 +6,56 @@ const urlsToCache = [
   "./style.css",
   "./app.js",
   "./firebase.js",
-  "./manifest.json"
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
-self.addEventListener("install", event => {
+// Install
+self.addEventListener("install", (event) => {
+  console.log("Service Worker Installed");
+
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
+    caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(urlsToCache);
     })
   );
+
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
+// Activate
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker Activated");
+
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            console.log("Deleting old cache:", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+// Fetch
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+    caches.match(event.request).then((response) => {
+      return (
+        response ||
+        fetch(event.request).catch(() => {
+          return caches.match("./index.html");
+        })
+      );
     })
   );
 });
